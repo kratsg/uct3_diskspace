38447b3f2459b454d5e48eeef78c739b9215cf8d
Giordon Stark <kratsg@gmail.com>
2015-05-19 01:42:09 -0500

    This will update to execute the duv.sh file copied over on the host.

--- getDiskUsage.sh
+++ getDiskUsage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /Users/giordon/uct3_diskspace
-ssh uc-tier3 'duv' > public/js/data.json
+ssh uc-tier3 './duv.sh' > public/js/data.json
 echo -e "{\"timestamp\":\""$(date)"\"}" > public/js/lastRun.json
