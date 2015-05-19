b325644e56e3446563c1a7994f5d5a3b331d78c2
Giordon Stark <kratsg@gmail.com>
2015-05-19 01:26:05 -0500

    Update getDiskUsage with permissions and bin/bash edeclation

--- getDiskUsage.sh
+++ getDiskUsage.sh
@@ -1,3 +1,4 @@
-/Users/giordon/uct3_diskspace
+#!/bin/bash
+cd /Users/giordon/uct3_diskspace
 ssh uc-tier3 'duv' > public/js/data.json
 echo -e "{\"timestamp\":\""$(date)"\"}" > public/js/lastRun.json
