6a1c3b2b49b68ecfeaf2568ecbd0919b398bae8f
Giordon Stark <kratsg@gmail.com>
2015-05-19 02:33:43 -0500

    also automatically commit and push changes

--- getDiskUsage.sh
+++ getDiskUsage.sh
@@ -2,3 +2,5 @@
 cd /Users/giordon/uct3_diskspace
 ssh uc-tier3 './duv.sh' > public/js/data.json
 echo -e "{\"timestamp\":\""$(date)"\"}" > public/js/lastRun.json
+git commit -am "cron job"
+git push
